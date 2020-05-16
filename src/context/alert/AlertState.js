05e9796a88aa6d3880e4893155edb65f920df114
Marina Solovieva <marishasolovieva@mail.ru>
2020-05-17 01:06:32 +0300

    react-hooks

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -6,10 +6,10 @@ import {HIDE_ALERT, SHOW_ALERT} from "../types";
 export const AlertState = ({children}) => {
     const [state, dispatch] = useReducer(alertReducer, null);
     const hide = () => dispatch( {type: HIDE_ALERT})
-    const show = (text, type="secondary") => dispatch( {type:SHOW_ALERT, payload: {text, type} })
+    const show = (text, type="danger") => dispatch( {type:SHOW_ALERT, payload: {text, type} });
     return (
         <AlertContext.Provider value={{hide, show, alert: state}}>
             {children}
         </AlertContext.Provider>
     )
-}
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,26 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Search} from "../components/Search";
 import {Card} from "../components/Card";
+import {GithubContext} from "../context/gitHub/githubContext";
 
 export const Home = () => {
-    const cards = new Array(15)
-        .fill(" ")
-        .map( (_, index) => index)
+    const {loading, users} = useContext(GithubContext);
+
     return (
         <React.Fragment>
             <Search/>
             <div className="row">
-                {cards.map( card => {
-                    return (
-                        <div className="col-sm-4 mb-4" key={card}>
-                            <Card/>
-                        </div>
-                    )
-                })}
-            </div>
+                {
+                    loading
+                        ? <p className="text-center">Загрузка...</p>
+                        : users.map(user => (
+                            <div className="col-sm-4 mb-4" key={user.id}>
+                                <Card user={user}/>
+                            </div>
+                        ))
+                }
 
+            </div>
         </React.Fragment>
     )
 }
